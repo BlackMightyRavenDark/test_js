bb3ffa6a47ba0e1f55afb62383cad913fbffa6be
BlackRain <masaraksh86@mail.ru>
2023-02-22 15:32:00 +0700

    test2

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-console.log("./../textures/font.png")
+const filePath = "./../textures/font.png";
+console.log(filePath);
+
+const img = new Image();
+img.onload = () => {
+    console.log(img.width, img.height);
+}
+img.src = filePath;
